292bfd212b3cf3e032a499f329c15393e5c80454
TheMuchi <michaelscheller1@gmail.com>
2023-10-19 15:56:22 +0200

    Add install scripts

---
+++ 1-install.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+#  ____                        ___           _        _ _
+# / ___|_      ____ _ _   _   |_ _|_ __  ___| |_ __ _| | |
+# \___ \ \ /\ / / _' | | | |   | || '_ \/ __| __/ _' | | |
+#  ___) \ V  V / (_| | |_| |   | || | | \__ \ || (_| | | |
+# |____/ \_/\_/ \__,_|\__, |  |___|_| |_|___/\__\__,_|_|_|
+#                     |___/
+# by darkylein (2023)
+# -----------------------------------------------------
+# Install Script for common packages, Sway and its required packages
+# ------------------------------------------------------
+
+# ------------------------------------------------------
+# Load Library
+# ------------------------------------------------------
+source "$(dirname "$0")/scripts/library.sh"
+clear
+echo ""
+echo "  ____                        ___           _        _ _"
+echo " / ___|_      ____ _ _   _   |_ _|_ __  ___| |_ __ _| | |"
+echo " \___ \ \ /\ / / _' | | | |   | || '_ \/ __| __/ _' | | |"
+echo "  ___) \ V  V / (_| | |_| |   | || | | \__ \ || (_| | | |"
+echo " |____/ \_/\_/ \__,_|\__, |  |___|_| |_|___/\__\__,_|_|_|"
+echo "                     |___/"
+echo "by darkylein (2023)"
+echo "-------------------------------------"
+echo ""
+
+# ------------------------------------------------------
+# Check if yay is installed
+# ------------------------------------------------------
+if sudo pacman -Qs yay > /dev/null ; then
+    echo "yay is installed. You can proceed with the installation"
+else
+    echo "yay is not installed. Will be installed now!"
+    _installPackagesPacman "base-devel"
+    git clone https://aur.archlinux.org/yay.git ~/yay
+    cd ~/yay || exit
+    makepkg -si
+    cd ~/dotfiles/ || exit
+    clear
+    echo ""
+    echo "  ____                        ___           _        _ _"
+    echo " / ___|_      ____ _ _   _   |_ _|_ __  ___| |_ __ _| | |"
+    echo " \___ \ \ /\ / / _' | | | |   | || '_ \/ __| __/ _' | | |"
+    echo "  ___) \ V  V / (_| | |_| |   | || | | \__ \ || (_| | | |"
+    echo " |____/ \_/\_/ \__,_|\__, |  |___|_| |_|___/\__\__,_|_|_|"
+    echo "                     |___/"
+    echo "by darkylein (2023)"
+    echo "-------------------------------------"
+    echo ""
+    echo "yay has been installed successfully."
+    echo ""
+fi
+
+# ------------------------------------------------------
+# Confirm Start
+# ------------------------------------------------------
+
+while true; do
+    read -r -p "DO YOU WANT TO START THE INSTALLATION NOW? (Yy/Nn): " yn
+    case $yn in
+        [Yy]* )
+            echo "Installation started."
+        break;;
+        [Nn]* )
+            exit;
+        break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+# ------------------------------------------------------
+# Install required packages
+# ------------------------------------------------------
+echo ""
+echo "-> Install main packages"
+
+packagesPacman=(
+    "alacritty"
+    "blueman"
+    "fuzzel"
+    "imv"
+    "mako"
+    "mpv"
+    "networkmanager"
+    "nano"
+    "neovim"
+    "polkit"
+    "starship"
+    "swaybg"
+    "swayidle"
+    "swaylock"
+    "thunar"
+    "ttf-nerd-fonts-symbols"
+    "vlc"
+    "waybar"
+    "xdg-user-dirs"
+    "xorg-xwayland"
+);
+
+packagesYay=(
+    "librewolf-bin"
+);
+
+_installPackagesPacman "${packagesPacman[@]}";
+_installPackagesYay "${packagesYay[@]}";
+
+# ------------------------------------------------------
+# DONE
+# ------------------------------------------------------
+clear
+echo "DONE!"
+echo "NEXT: Please continue with 2-install-hyprland.sh and/or 2-install-qtile.sh"
---
+++ 2-dotfiles.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+#      _       _    __ _ _              ___           _        _ _
+#   __| | ___ | |_ / _(_) | ___  ___   |_ _|_ __  ___| |_ __ _| | |
+#  / _' |/ _ \| __| |_| | |/ _ \/ __|   | || '_ \/ __| __/ _' | | |
+# | (_| | (_) | |_|  _| | |  __/\__ \   | || | | \__ \ || (_| | | |
+#  \__,_|\___/ \__|_| |_|_|\___||___/  |___|_| |_|___/\__\__,_|_|_|
+#
+# by darkylein (2023)
+# ------------------------------------------------------
+# Install Script for dotfiles and configuration
+# yay must be installed
+# ------------------------------------------------------
+
+# ------------------------------------------------------
+# Load Library
+# ------------------------------------------------------
+source "$(dirname "$0")"/scripts/library.sh
+clear
+echo ""
+echo "      _       _    __ _ _              ___           _        _ _"
+echo "   __| | ___ | |_ / _(_) | ___  ___   |_ _|_ __  ___| |_ __ _| | |"
+echo "  / _' |/ _ \| __| |_| | |/ _ \/ __|   | || '_ \/ __| __/ _' | | |"
+echo " | (_| | (_) | |_|  _| | |  __/\__ \   | || | | \__ \ || (_| | | |"
+echo "  \__,_|\___/ \__|_| |_|_|\___||___/  |___|_| |_|___/\__\__,_|_|_|"
+echo ""
+echo "by darkylein (2023)"
+echo "-------------------------------------"
+echo ""
+echo "The script will ask for permission to remove existing directories and files from ~/.config/"
+echo "Symbolic links will then be created from ~/dotfiles into your ~/.config/ directory."
+echo "But you can decide to keep your personal versions by answering with No (Nn)."
+echo ""
+
+# ------------------------------------------------------
+# Confirm Start
+# ------------------------------------------------------
+while true; do
+    read -r -p "DO YOU WANT TO START THE INSTALLATION NOW? (Yy/Nn): " yn
+    case $yn in
+        [Yy]* )
+            echo "Installation started."
+        break;;
+        [Nn]* )
+            exit;
+        break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+# ------------------------------------------------------
+# Create .config folder
+# ------------------------------------------------------
+echo ""
+echo "-> Check if .config folder exists"
+
+if [ -d ~/.config ]; then
+    echo ".config folder already exists."
+else
+    mkdir ~/.config
+    echo ".config folder created."
+fi
+
+# ------------------------------------------------------
+# Create symbolic links
+# ------------------------------------------------------
+# name symlink source target
+
+echo ""
+echo "-------------------------------------"
+echo "-> Install general dotfiles"
+echo "-------------------------------------"
+echo ""
+
+_installSymLink fuzzel ~/.config/fuzzel ~/dotfiles/fuzzel/ ~/.config
+
+#echo "-------------------------------------"
+#echo "-> Install GTK dotfiles"
+#echo "-------------------------------------"
+#echo ""
+#
+#_installSymLink .gtkrc-2.0 ~/.gtkrc-2.0 ~/dotfiles/gtk/.gtkrc-2.0 ~/.gtkrc-2.0
+#_installSymLink gtk-3.0 ~/.config/gtk-3.0 ~/dotfiles/gtk/gtk-3.0/ ~/.config/
+#_installSymLink gtk-4.0 ~/.config/gtk-4.0 ~/dotfiles/gtk/gtk-4.0/ ~/.config/
+
+echo "-------------------------------------"
+echo "-> Install Sway dotfiles"
+echo "-------------------------------------"
+echo ""
+
+_installSymLink sway ~/.config/sway ~/dotfiles/sway/ ~/.config
+_installSymLink swaylock ~/.config/swaylock ~/dotfiles/swaylock/ ~/.config
+_installSymLink waybar ~/.config/waybar ~/dotfiles/waybar/ ~/.config
+
+# ------------------------------------------------------
+# DONE
+# ------------------------------------------------------
+echo "DONE!"
+echo "NEXT: Please logout and reboot your system!"

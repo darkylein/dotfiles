ba4b6a50c02535e7d17f68de3e21309769f3bfd9
darkylein <michaelscheller1@gmail.com>
2023-10-21 21:34:37 +0200

    Add icon scheme to gtk script

--- gtk/gtk.sh
+++ gtk/gtk.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-gsettings set org.gnome.desktop.interface gtk-theme 'Dracula'
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+gsettings set org.gnome.desktop.interface gtk-theme 'Dracula'
+gsettings set org.gnome.desktop.interface icon-theme 'Dracula'

a693d9fda113d51e0ee973861bee17d07cf734f5
darkylein <michaelscheller1@gmail.com>
2024-01-05 01:36:40 +0100

    Simplify swayidle and replace shutdown_menu with session_menu

--- scripts/gtk.sh
+++ scripts/gtk.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 gsettings set org.gnome.desktop.interface gtk-theme 'Nordic-darker'
 gsettings set org.gnome.desktop.wm.preferences theme 'Nordic-darker'
